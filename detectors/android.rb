1ab8ec6cccb536cd844e1b954ebac653dcd58a27
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-03-05 16:36:50 +0100

    use the most root gradlew file

--- detectors/android.rb
+++ detectors/android.rb
@@ -2,8 +2,8 @@ require_relative '../config_helper'
 
 branch = ARGV[0]
 unless branch
-	puts "\e[31mBranch not specified\e[0m"
-	exit 0
+  puts "\e[31mBranch not specified\e[0m"
+  exit 0
 end
 
 gradle_files = Dir.glob(File.join("**/build.gradle"), File::FNM_CASEFOLD)
@@ -18,66 +18,80 @@ config_helper = ConfigHelper.new
 gradlew_files = Dir.glob('./**/gradlew', File::FNM_CASEFOLD)
 
 gradlew_path = nil
-gradlew_path = gradlew_files[0] if !gradlew_files.nil? && gradlew_files.count == 1
+
+if !gradlew_files.nil? && gradlew_files.count > 0
+  gradlew_files.each do |gradlew|
+    if gradlew_path.nil?
+      gradlew_path = gradlew
+    else
+      new_splits = gradlew.split(File::SEPARATOR)
+      splits = gradlew_path.split(File::SEPARATOR)
+
+      if !splits.nil? && !new_splits.nil? && splits.count > new_splits.count
+        gradlew_path = gradlew
+      end
+    end
+  end
+end
 
 gradlew_or_gradle = 'gradle'
 if gradlew_path
-	puts
-	puts " -> Gradle wrapper (gradlew) found - using it: #{gradlew_path}"
-	gradlew_or_gradle = gradlew_path
+  puts
+  puts " -> Gradle wrapper (gradlew) found - using it: #{gradlew_path}"
+  gradlew_or_gradle = gradlew_path
 
-	unless File.executable?(gradlew_or_gradle)
-		puts " (i) Missing executable permission on gradlew file, adding it now. Path: #{gradlew_or_gradle}"
+  unless File.executable?(gradlew_or_gradle)
+    puts " (i) Missing executable permission on gradlew file, adding it now. Path: #{gradlew_or_gradle}"
 
-		File.chmod(0770, gradlew_or_gradle)
-	end
+    File.chmod(0770, gradlew_or_gradle)
+  end
 else
-	puts
-	puts " -> No gradle wrapper (gradlew) found - using gradle directly"
+  puts
+  puts " -> No gradle wrapper (gradlew) found - using gradle directly"
 end
 
 puts
 puts "gradlew_or_gradle: #{gradlew_or_gradle}"
 
 gradle_files.each do |gradle_file|
-	configurations = []
+  configurations = []
 
-	puts ""
-	puts "\e[32mInspecting gradle file at path: #{gradle_file}\e[0m"
-	puts " -> Inspecting output of: $ #{gradlew_or_gradle} tasks --build-file '#{gradle_file}'"
-	IO.popen "#{gradlew_or_gradle} tasks --build-file '#{gradle_file}'" do |io|
-		is_build_tasks_section = false
-		io.each do |line|
-			if !is_build_tasks_section && line.match(/^Build tasks/)
-				is_build_tasks_section = true
-				next
-			else
-				if line.strip.empty?
-					is_build_tasks_section = false
-					next
-				end
-			end
-			if is_build_tasks_section
-				next if line.start_with? '--'
-				if match = line.match(/^(?<configuration>assemble\S+)(\s*-\s*.*)*/)
-					(configurations ||= []) << match.captures[0]
-				end
-			end
-		end
-	end
+  puts ""
+  puts "\e[32mInspecting gradle file at path: #{gradle_file}\e[0m"
+  puts " -> Inspecting output of: $ #{gradlew_or_gradle} tasks --build-file '#{gradle_file}'"
+  IO.popen "#{gradlew_or_gradle} tasks --build-file '#{gradle_file}'" do |io|
+    is_build_tasks_section = false
+    io.each do |line|
+      if !is_build_tasks_section && line.match(/^Build tasks/)
+        is_build_tasks_section = true
+        next
+      else
+        if line.strip.empty?
+          is_build_tasks_section = false
+          next
+        end
+      end
+      if is_build_tasks_section
+        next if line.start_with? '--'
+        if match = line.match(/^(?<configuration>assemble\S+)(\s*-\s*.*)*/)
+          (configurations ||= []) << match.captures[0]
+        end
+      end
+    end
+  end
 
-	puts "Assemble configurations (except 'assemble'):"
-	if configurations.count > 0
-		configurations.each { |configuration| puts configuration }
-		puts "#{configurations.count} in total"
+  puts "Assemble configurations (except 'assemble'):"
+  if configurations.count > 0
+    configurations.each { |configuration| puts configuration }
+    puts "#{configurations.count} in total"
 
-		config_helper.save("android", branch, {
-			name: gradle_file,
-			path: gradle_file,
-			schemes: configurations,
-			gradlew_path: gradlew_path
-		})
-	else
-		puts "\e[31mNo configuration found\e[0m"
-	end
+    config_helper.save("android", branch, {
+      name: gradle_file,
+      path: gradle_file,
+      schemes: configurations,
+      gradlew_path: gradlew_path
+    })
+  else
+    puts "\e[31mNo configuration found\e[0m"
+  end
 end
